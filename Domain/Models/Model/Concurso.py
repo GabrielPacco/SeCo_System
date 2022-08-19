8747d1f96ea2b195a7ec1b455d54c4f8acca5fff
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2022-08-18 21:47:35 -0500

    Delete Domain/Models/Model directory

--- Domain/Models/Model/Concurso.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Concurso:
-    def __init__(self):
-        self.participante = None
-        self.base = None
-        self.premio = None
-
--- Domain/Models/Model/Ponencia.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Ponencia:
-    def __init__(self):
-        self.topico = None
-
