0732f5b3617e05c668ac5c3b6eed9157e96ebf89
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2022-08-18 21:07:19 -0500

    Delete models directory

--- models/Course.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Course:
-    def __init__(self):
-        self.participants = None
-        self.bases = None
-        self.prize = None
-
--- models/Panel.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Panel:
-    def __init__(self):
-        self.topic = None
-
