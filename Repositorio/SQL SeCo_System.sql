b35487e54c9ae2a6a7dd48a528a5bac399da6354
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2022-08-20 19:58:44 -0500

    Add files via upload

---
+++ Repositorio/SQL SeCo_System.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE BBDDSECOSISTEM;
+USE  BBDDSECOSISTEM;
+
+CREATE TABLE  usuario
+(
+	id_user INT PRIMARY KEY ,
+    nombre VARCHAR(50),
+    usuario VARCHAR(50),
+    contrasenia VARCHAR(50),
+    email VARCHAR(50),
+    telefono INT
+);
+
+CREATE TABLE invitado
+(
+	id_inv INT,
+    id_user INT,
+    universidad  VARCHAR(50),
+    carrera  VARCHAR(50),
+    grado  VARCHAR(20),
+    PRIMARY KEY (id_user,id_inv),
+    FOREIGN KEY (id_user) REFERENCES usuario (id_user)
+);
+CREATE TABLE administrador
+(
+	id_adm INT,
+    id_user INT,
+    PRIMARY KEY (id_user,id_adm),
+    FOREIGN KEY (id_user) REFERENCES usuario (id_user)
+);
+CREATE TABLE contribuidor
+(
+	id_cont INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    universidad  VARCHAR(50),
+    especialidad VARCHAR(50),
+    descripcion TEXT,
+    facebool VARCHAR(50),
+    email VARCHAR(50),
+    linkedin VARCHAR(50),
+    rol VARCHAR(20)
+);
+CREATE TABLE actividad
+(
+    id_act INT PRIMARY KEY,
+    nombre VARCHAR(30),
+    fecha DATE,
+    hora_inicio TIME,
+    hora_fin TIME,
+    estado VARCHAR(30),
+    enlace_reu TEXT
+);
+CREATE TABLE edicion 
+(
+	id_edi INT,
+    id_act INT,
+    anho INT,
+    nombre VARCHAR(30),
+    PRIMARY KEY (id_edi,id_act),
+    FOREIGN KEY (id_act) REFERENCES actividad (id_act)
+);
+CREATE TABLE concurso 
+(	
+	id_conc INT,
+    id_act INT,
+    participante VARCHAR(40),
+    base TEXT,
+    premio VARCHAR(30),
+	PRIMARY KEY (id_conc,id_act),
+    FOREIGN KEY (id_act) REFERENCES actividad (id_act)
+);
+CREATE TABLE ponencia 
+(
+	id_pon INT,
+    id_act INT,
+    topico VARCHAR(30),
+	PRIMARY KEY (id_pon,id_act),
+    FOREIGN KEY (id_act) REFERENCES actividad (id_act)
+);
+CREATE TABLE protocolar 
+(
+	id_prot INT,
+    id_act INT,
+    regla TEXT,
+	PRIMARY KEY (id_prot,id_act),
+    FOREIGN KEY (id_act) REFERENCES actividad (id_act)
+);
+CREATE TABLE panel 
+(
+	id_pan INT,
+    id_act INT,
+    topico VARCHAR(30),
+	PRIMARY KEY (id_pan,id_act),
+    FOREIGN KEY (id_act) REFERENCES actividad (id_act)
+);
