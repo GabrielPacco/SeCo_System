731b9c0f13b4cb646ce62e1da49559b3fb92f47c
UbertoGC <84751424+UbertoGC@users.noreply.github.com>
2022-08-27 16:54:24 -0500

    Update Contribuidor.py

--- Dominio/Modelo/Contribuidor.py
+++ Dominio/Modelo/Contribuidor.py
@@ -2,14 +2,13 @@
 #-*- coding: utf-8 -*-
 
 class Contribuidor:
-    def __init__(self):
-        self.id = None
-        self.nombre = None
-        self.universidad = None
-        self.especialidad = None
-        self.descripcion = None
-        self.email = None
-        self.facebook = None
-        self.linkedin = None
-        self.rol = None
-
+    def __init__(self, id_1,nom_1,uni_1,esp_1,des_1,ema_1,fac_1,lin_1,rol_1):
+        self.id = id_1
+        self.nombre = nom_1
+        self.universidad = uni_1
+        self.especialidad = esp_1
+        self.descripcion = des_1
+        self.email = ema_1
+        self.facebook = fac_1
+        self.linkedin = lin_1
+        self.rol = rol_1
