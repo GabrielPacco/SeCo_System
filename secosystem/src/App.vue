c0ddea6843fff75db270d1f9c80a19c4fb6707e5
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-08-28 18:37:48 -0500

    Eliminado comentarios

--- secosystem/src/App.vue
+++ secosystem/src/App.vue
@@ -11,8 +11,8 @@
 <style>
 #building{
 background:url("../assets/image/BJ1.jpg");
-width:100%;			// El tamaño se establece en 100%
-height:100%;			// El tamaño se establece en 100%
+width:100%;			
+height:100%;			
 position:fixed;
 background-size:100% 100%;}
 </style>
