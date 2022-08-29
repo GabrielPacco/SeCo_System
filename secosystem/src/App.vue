7ee459dd251342755f36c8bd4982940b734ef6b9
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-08-28 19:42:56 -0500

    Eliminando parte causante del error

--- secosystem/src/App.vue
+++ secosystem/src/App.vue
@@ -7,15 +7,7 @@
 </template>
 <div id="building">
 </div>
-  
-<style>
-#building{
-background:url("../assets/image/BJ1.jpg");
-width:100%;			
-height:100%;			
-position:fixed;
-background-size:100% 100%;}
-</style>
+
 <script>
 
 
