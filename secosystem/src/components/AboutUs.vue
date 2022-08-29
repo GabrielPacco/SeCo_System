151d41e279bef61d9452f782977ae992e6fc8fb2
Erick-Perez12 <82849144+Erick-Perez12@users.noreply.github.com>
2022-08-29 02:47:22 -0500

    Update AboutUs.vue

--- secosystem/src/components/AboutUs.vue
+++ secosystem/src/components/AboutUs.vue
@@ -176,47 +176,6 @@ En esta ocasión, el proyecto desarrollado es referido a "La semana de computaci
   </v-app>
 </template>
 
-<!-- Agregar item-->
-<template>
-  <v-dialog v-model="dialog" max-width="500px">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-        Agregar Item
-      </v-btn>
-    </template>
-    <v-card>
-      <v-card-title>
-        <span class="headline">Agregar Item</span>
-      </v-card-title>
-
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="editedItem.nombre" label="Nombre"></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="editedItem.precio" label="Precio"></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="editedItem.cantidad" label="Cantidad"></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="close">Cancelar</v-btn>
-        <v-btn color="blue darken-1" text @click="save">Guardar</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-</template>
-
-
-
-
 <script>
 export default {
   data: () => ({
